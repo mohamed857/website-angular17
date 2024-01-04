bae7cd482c2a679f2b4113886215b1eac8228c8d
mohamed857 <elcessym@gmail.com>
2024-01-05 01:41:56 +0200

    edit

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -9,9 +9,9 @@ import { Component } from '@angular/core';
 })
 export class TestimonialsComponent {
 feedbacks=[
-  {id:1,src:'./assets/images/user-images/user-1.jpg',opinion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utlaboreet dolore magna aliqua',name:'Ghareeb sayed - Happy Customer'},
-  {id:1,src:'./assets/images/user-images/user-2.jpg',opinion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utlaboreet dolore magna aliqua',name:'Mohamed Zain - Happy Customer'},
-  {id:1,src:'./assets/images/user-images/user-3.jpg',opinion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utlaboreet dolore magna aliqua',name:'Walid Mohamed - Happy Customer'},
+  {id:1,src:'./assets/images/user-images/ghareb.enc',opinion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utlaboreet dolore magna aliqua',name:'Ghareeb sayed - Happy Customer'},
+  {id:1,src:'./assets/images/user-images/zain.jpg',opinion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utlaboreet dolore magna aliqua',name:'Mohamed Zain - Happy Customer'},
+  {id:1,src:'./assets/images/user-images/walid.jpg',opinion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utlaboreet dolore magna aliqua',name:'Walid Mohamed - Happy Customer'},
 
 ]
 }
